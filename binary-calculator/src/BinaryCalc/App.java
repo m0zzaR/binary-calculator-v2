c662835b7ad937cc40241d5e7051ec9b32f9796d
m0zzaR <gabrielluisfonseca@gmail.com>
2021-05-05 20:41:14 -0700

    Update App.java

--- binary-calculator/src/BinaryCalc/App.java
+++ binary-calculator/src/BinaryCalc/App.java
@@ -1,47 +1,89 @@
 package BinaryCalc;
 import java.util.*;
+
+import javax.lang.model.util.ElementScanner14;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
 
         String binNum = "0";
         boolean flag = true;
+        boolean calcFlag = true;
 
-        System.out.println("Input binary number");
-
-        while (flag) {
         
-            
-            
-                    binNum = scan.nextLine();
-                    if (binNum.equals("69")) {
-                        System.exit(0);
-                    }
 
-                    for (int i = 0; i < binNum.length(); i++) {
 
-                        if (!(binNum.substring(i, i + 1).equals("0") || binNum.substring(i, i + 1).equals("1"))) {
+        while (calcFlag) {
 
-                            System.out.println();
-                            System.out.println("Please input only a binary number");
-                            
+            System.out.println("Would you like to calculate binary to whole, or whole to binary?");
+            System.out.println("Print B for binary calc and W for whole number calc");
+            String ans = scan.nextLine();
+            ans = ans.toLowerCase();
 
-                            flag = true;
-                            break;
-                        }
-                        else
-                        flag = false;
+        if (ans.equals("w")) {
+        
+            System.out.println();
+            System.out.println("Input whole number");
+            System.out.println();
+            int whoNum = scan.nextInt();
 
-                    }
+                BinCalc who = new BinCalc();
+                who.wholeCalc(whoNum);
+
+            calcFlag = false;
+
+                }
+                else if (ans.equals("b")) {
+                
+
+
+                System.out.println();
+                System.out.println("Input binary number");
+                System.out.println();
+                
+
+
+                while (flag) {
+                
+                    
+                    
+                            binNum = scan.nextLine();
+                            if (binNum.equals("69")) {
+                                System.exit(0);
+                            }
+
+                            for (int i = 0; i < binNum.length(); i++) {
+
+                                if (!(binNum.substring(i, i + 1).equals("0") || binNum.substring(i, i + 1).equals("1"))) {
+
+                                    System.out.println();
+                                    System.out.println("Please input only a binary number");
+                                    
+
+                                    flag = true;
+                                    break;
+                                }
+                                else
+                                flag = false;
+
+                            }
+                }
+
+                BinCalc bin = new BinCalc();
+                int num = bin.binCalc(binNum);
+                    
+                    
+                System.out.println();
+                System.out.println(num);
+                System.out.println();
+                calcFlag = false;
+
         }
-
-        BinCalc bin = new BinCalc();
-        int num = bin.binCalc(binNum);
-            System.out.println("Hello");
-            
+        else {
+        System.out.println("That is not B or W");
         System.out.println();
-        System.out.println(num);
-
+        }
+    }
         scan.close();
     }
 }
