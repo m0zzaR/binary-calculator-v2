71dad94bd9301afb5e6769b7dc17794368690c46
m0zzaR <gabrielluisfonseca@gmail.com>
2021-05-05 20:44:24 -0700

    Update App.java

--- binary-calculator/src/BinaryCalc/App.java
+++ binary-calculator/src/BinaryCalc/App.java
@@ -1,7 +1,5 @@
 package BinaryCalc;
 import java.util.*;
-
-import javax.lang.model.util.ElementScanner14;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
